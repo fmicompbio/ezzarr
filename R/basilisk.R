acdddd2fd6e32206ecd64ebc164f28dcd3c1a0d4
Charlotte Soneson <charlottesoneson@gmail.com>
2025-11-05 19:31:59 +0100

    Adapt to pyenv-based basilisk (Bioc 3.22)

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,16 +1,12 @@
 #' @importFrom basilisk BasiliskEnvironment
 
-.conda_dependencies <- c(
-    "python==3.12"
-)
 .pip_dependencies <- c(
+    "python==3.12",
     "ez_zarr==0.4.2"
 )
 
 ezzarrenv <- BasiliskEnvironment(
     envname = "ezzarr", pkgname = "ezzarr",
-    packages = .conda_dependencies,
-    channels = c("conda-forge"),
-    pip = .pip_dependencies
+    packages = .pip_dependencies
 )
 
